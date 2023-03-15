1a99ad0a07444c2877b57c874ce0c0e235030649
palmpaybalanceadder <121066576+palmpaybalanceadder@users.noreply.github.com>
2023-03-15 10:04:23 +0100

    ib_chat.py

--- harshitethic.py
+++ harshitethic.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "6088007602:AAHRhD4FT8Y7k7_DGvwUE7zo86nv_PBuymw"
 else:
     tgkey = tgenv
 print(tgenv)
